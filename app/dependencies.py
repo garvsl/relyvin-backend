fbfa51a881cd4c5c82ae24ad2aa68a7dad45d4bb
garvsl <garvsehgal7@gmail.com>
2024-09-07 11:51:11 -0400

    auth header user retrieval

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,4 +1,7 @@
-from fastapi import Depends
+import datetime
+import json
+from fastapi import Depends, HTTPException, Request
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from prisma import Prisma
 
 prisma = Prisma()
@@ -8,6 +11,11 @@ async def get_db() -> Prisma:
 
 DB = Depends(get_db)
 
+security = HTTPBearer()
 
+async def get_current_user(request:Request, credentials:HTTPAuthorizationCredentials = Depends(security)):
+    if not hasattr(request.state, 'session'):
+        raise HTTPException(status_code=401, detail="Auth Required")
+    return request.state.session['user_id']
 
-    
+USER = Depends(get_current_user)
