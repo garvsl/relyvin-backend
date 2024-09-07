53befec538da0e4744db18355720192d33627e9e
garvsl <garvsehgal7@gmail.com>
2024-09-07 11:50:59 -0400

    session storing middleware

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
-from fastapi import Depends, FastAPI
+import datetime
+import json
+from fastapi import Depends, FastAPI, HTTPException, Request
 from typing import Union
 from contextlib import asynccontextmanager
+from fastapi.responses import JSONResponse
 from prisma import Prisma
 from routers import users
 from app.dependencies import prisma
@@ -16,6 +19,31 @@ async def lifespan(app:FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+
+@app.middleware("http")
+async def session_middleware(request: Request, call_next):
+    session_id = request.headers.get("Authorization")
+
+    if not session_id:
+        return await call_next(request)
+
+    session = await prisma.session.find_unique(
+        where={
+            'sid':session_id
+        }
+    )
+    
+    if not session or session.expiresAt < datetime.now(datetime.UTC):
+        return JSONResponse(
+            status_code=401,
+            content={"detail":"Invalid or expired session"}
+        )
+    
+    request.state.session = json.loads(session.data)    
+    response = await call_next(request)
+    return response
+    
+
 
 @app.get("/")
 def read_root():
