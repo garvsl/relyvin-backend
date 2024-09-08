f1cdb4f76cc4b51cf504e6d72c667f937def516d
garvsl <garvsehgal7@gmail.com>
2024-09-07 22:52:01 -0400

    get all scripts

--- app/prisma/db/script.py
+++ app/prisma/db/script.py
@@ -4,7 +4,7 @@ from prisma import Prisma
 
 
 async def get_scripts(id: str, prisma:Prisma):
-    return await prisma.script.find_first(
+    return await prisma.script.find_many(
         where={
             'userId': id
         },
@@ -12,3 +12,13 @@ async def get_scripts(id: str, prisma:Prisma):
         'created': 'desc',
         },
     )
+
+# async def update_script(id: str, prisma:Prisma):
+#     return await prisma.script.find_first(
+#         where={
+#             'id': id
+#         },
+#         order={
+#         'created': 'desc',
+#         },
+#     )
