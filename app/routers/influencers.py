77b6997da4fbd8da1357e65b17d428f74eb586b3
garvsl <garvsehgal7@gmail.com>
2024-09-08 00:10:41 -0400

    influencer routes

---
+++ app/routers/influencers.py
@@ -0,0 +1,25 @@
+
+from typing import Optional
+from fastapi import APIRouter
+from prisma import Prisma
+
+from app.dependencies import DB, USER
+from app.prisma.db.influencer import get_influencer, get_influencers, get_influencers_count
+
+
+router = APIRouter()
+
+
+@router.get("/influencers/", tags=["influencers"])
+async def read_influencers(db: Prisma = DB, user:str = USER, skip:Optional[int] = 0 ):
+    return await get_influencers(db, skip)
+
+@router.get("/influencers/count", tags=["influencers"])
+async def read_influencers_count(db: Prisma = DB, user:str = USER):
+    return await get_influencers_count(db)
+
+@router.get("/influencers/find", tags=["influencers"])
+async def find_influencer(handle:Optional[str] = None, email:Optional[str] = None, db: Prisma = DB, user:str = USER):
+    return await get_influencer(db, handle, email)
+    
+    
