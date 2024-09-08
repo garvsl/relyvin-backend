5598f6a564a0effeb4168b49b1a9682cefa75d24
garvsl <garvsehgal7@gmail.com>
2024-09-08 00:28:18 -0400

    usernames route

---
+++ app/prisma/db/username.py
@@ -0,0 +1,12 @@
+from typing import Optional
+from prisma import Prisma
+
+
+async def get_usernames(prisma:Prisma, skip:Optional[int] = 0 ):
+    return await prisma.username.find_many(
+        take=100,
+        skip=skip
+    )
+
+async def get_usernames_count(prisma:Prisma):
+    return await prisma.username.count()
---
+++ app/routers/usernames.py
@@ -0,0 +1,18 @@
+from typing import Optional
+from fastapi import APIRouter
+from prisma import Prisma
+
+from app.dependencies import DB, USER
+from app.prisma.db.username import get_usernames, get_usernames_count
+
+
+router = APIRouter()
+
+
+@router.get("/usernames/", tags=["usernames"])
+async def read_usernames(db: Prisma = DB, user:str = USER, skip:Optional[int] = 0 ):
+    return await get_usernames(db, skip)
+
+@router.get("/usernames/count", tags=["usernames"])
+async def read_usernames_count(db: Prisma = DB, user:str = USER):
+    return await get_usernames_count(db)
